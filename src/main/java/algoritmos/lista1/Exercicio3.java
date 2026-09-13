238ebc1e0290fc92ad6181fa8e7117a689b6e1e5
Fabricio Lopes <prog.fabriciolopes@gmail.com>
2026-09-13 00:33:28 -0300

    Algoritmos - Lista 1 - Exercicio 3

---
+++ src/main/java/algoritmos/lista1/Exercicio3.java
@@ -0,0 +1,24 @@
+package algoritmos.lista1;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite a distância percorrida (KM): ");
+		float distancia = sc.nextFloat();
+		
+		System.out.print("Digite a quantidade de combustível gasto (Litro(s)): ");
+		float litros = sc.nextFloat();
+		
+		sc.close();
+		
+		float media = distancia / litros;
+		
+		System.out.println("A média de consumo é: "+media+" km/l.");
+	}
+	
+}
