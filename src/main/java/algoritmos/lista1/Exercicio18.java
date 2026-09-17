627450937b401998160fa0b5118def8221a7e097
Fabricio Lopes <prog.fabriciolopes@gmail.com>
2026-09-16 21:59:41 -0300

    Add - Lista 1 - Exercicio 18

---
+++ src/main/java/algoritmos/lista1/Exercicio18.java
@@ -0,0 +1,27 @@
+package algoritmos.lista1;
+
+public class Exercicio18 {
+
+	public static void main(String[] args) {
+		// Faça um algoritmo que receba a idade de 75 pessoas e mostre mensagem informando “maior de idade” e “menor de idade” para cada pessoa. Considere a idade a partir de 18 anos como maior de idade;
+
+		
+		/* NÃO IREI FAZER, EXIGE MUITO TEMPO PARA PREENCHER AS 75 PESSOAS, MAS A LÓGICA É SIMPLES:
+		 * 	UM LOOP EXECUTA 75 VEZES, PARA CADA EXERCUÇÃO FAZER O SEGUINTE:
+		 * - SOLICITAR NOME
+		 * - SOLICITAR IDADE
+		 * - VERIFICAR SE IDADE É >= 18 ----> SE SIM (println("Maior de idade"); SE NÃO (println("Menor de idade")
+		 * 
+		 * LEMBRANDO QUE O LOOP TEM QUE TER UM ITEM PARA ITERAR E TER FIM:
+		 * 
+		 * int i = 0
+		 * while (i < 75){
+		 * 
+		 * 		ALGORITMO AQUI...
+		 * 
+		 * 		i = i + 1;
+		 * }
+		 */
+	}
+
+}
