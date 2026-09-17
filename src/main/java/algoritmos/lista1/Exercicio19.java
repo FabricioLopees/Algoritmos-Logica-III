9577364d57013df5ecefd0cb0e04454a9fbbbc34
Fabricio Lopes <prog.fabriciolopes@gmail.com>
2026-09-16 21:59:48 -0300

    Add - Lista 1 - Exercicio 19

---
+++ src/main/java/algoritmos/lista1/Exercicio19.java
@@ -0,0 +1,11 @@
+package algoritmos.lista1;
+
+public class Exercicio19 {
+
+	public static void main(String[] args) {
+		// Escreva um algoritmo que leia o nome e o sexo de 56 pessoas e informe o nome e se ela é homem ou mulher. No final informe total de homens e de mulheres;
+		
+		// MESMA COISA DO EXERCÍCIO ANTERIOR
+	}
+
+}
